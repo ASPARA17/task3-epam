f6fae1c50b70dd1118b7d5dbe4a3a35aa372ee46
Sofya Prokopovich <sofya0804@gmail.com>
2021-09-23 22:13:10 +0300

    added ResourceException class

---
+++ multithreading/src/main/java/com/epam/jwd/service/exception/ResourceException.java
@@ -0,0 +1,9 @@
+package com.epam.jwd.service.exception;
+
+public class ResourceException extends Throwable{
+    public ResourceException() {};
+
+    public ResourceException(String message) {
+        super(message);
+    }
+}
