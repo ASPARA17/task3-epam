0bbed06543e46055b70d72aea1ff615f81b639bd
Sofya Prokopovich <sofya0804@gmail.com>
2021-09-27 20:49:06 +0300

    add logger in IdGenerator

--- multithreading/src/main/java/com/epam/jwd/service/logic/IdGenerator.java
+++ multithreading/src/main/java/com/epam/jwd/service/logic/IdGenerator.java
@@ -1,9 +1,16 @@
 package com.epam.jwd.service.logic;
 
+import com.epam.jwd.service.impl.CallOperator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class IdGenerator{
+    private static final Logger logger = LogManager.getLogger(IdGenerator.class);
+    private static final String GENERATE_ID = "Generate id";
     private static Long id = 1L;
 
     public static Long generateId() {
+        logger.debug(GENERATE_ID);
         return ++id;
     }
 }
