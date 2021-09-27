c69f36df4320ee90a7f1bd410ad532a77da7433c
Sofya Prokopovich <sofya0804@gmail.com>
2021-09-27 20:48:19 +0300

    upd Client

--- multithreading/src/main/java/com/epam/jwd/service/logic/Client.java
+++ multithreading/src/main/java/com/epam/jwd/service/logic/Client.java
@@ -5,26 +5,58 @@ import com.epam.jwd.service.impl.CallOperator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Client implements Runnable {
+import java.util.Objects;
+
+public class Client extends Thread {
     private static final Logger logger = LogManager.getLogger(Client.class);
+    private static final String CONNECT_OPERATOR = "Client connected to operator";
+    private static final String DISCONNECT_OPERATOR = "Client disconnected to operator";
+    private final int MAX_WAIT_MILLIS = 500;
     private OperatorPool<CallOperator> pool;
+    private Long id;
 
     public Client(OperatorPool<CallOperator> pool) {
         this.pool = pool;
+        this.id = IdGenerator.generateId();
+    }
+
+    public long getId() {
+        return id;
     }
 
     @Override
     public void run() {
         CallOperator operator = null;
         try {
-            operator = pool.getResource(this, 500);
+            logger.debug(CONNECT_OPERATOR);
+            operator = pool.getOperator(this, MAX_WAIT_MILLIS);
             operator.service();
         } catch (ResourceException e) {
-            logger.error(e.getMessage());
+            logger.error(e);
+            System.err.println("Client #" + this.getId() + " not wait ->"+ e.getMessage());
         } finally {
             if (operator != null) {
-                pool.unlockResource(this, operator);
+                logger.debug(DISCONNECT_OPERATOR);
+                pool.releaseOperator(this, operator);
             }
         }
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" + "id=" + id + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return id.equals(client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
