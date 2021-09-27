bc7ff659cf92fdff4cdc6fbd98fac690c873befb
Sofya Prokopovich <sofya0804@gmail.com>
2021-09-27 20:50:11 +0300

    upd OperatorPool

--- multithreading/src/main/java/com/epam/jwd/service/logic/OperatorPool.java
+++ multithreading/src/main/java/com/epam/jwd/service/logic/OperatorPool.java
@@ -14,23 +14,27 @@ public class OperatorPool<T extends Operator> {
     private static final Logger logger = LogManager.getLogger(OperatorPool.class);
     private static final String TRY_LOCK = "Attempt to block";
     private static final String UNLOCK = "Unlock thread";
+    private static final String LOCK_THREAD = "Blocking a thread";
 
     private final static int POOL_SIZE = 5;
     private final Semaphore semaphore = new Semaphore(POOL_SIZE, true);
-    private final ArrayList<T> resources = new ArrayList<>();
+    private final ArrayList<T> operators = new ArrayList<>();
 
     public OperatorPool(List<T> source) {
-        resources.addAll(source);
+        operators.addAll(source);
     }
 
-    public T getResource(Client client, long maxWaitMillis) throws ResourceException {
+    public T getOperator(Client client, long maxWaitMillis) throws ResourceException {
         try {
             logger.debug(TRY_LOCK);
             if (semaphore.tryAcquire(maxWaitMillis, TimeUnit.MILLISECONDS)) {
-                for (T resource : resources) {
-                    if (!resource.isBusy()) {
-                        resource.setBusy(true);
-                        return resource;
+                for (T operator : operators) {
+                    if (!operator.isBusy()) {
+                        logger.debug(LOCK_THREAD + operator);
+                        operator.setBusy(true);
+                        System.out.println("Client #" + client.getId()
+                                + " connected to operator " + operator);
+                        return operator;
                     }
                 }
             }
@@ -38,14 +42,14 @@ public class OperatorPool<T extends Operator> {
             Thread.currentThread().interrupt();
             logger.error(e);
         }
-        // TODO: 19.09.2021 add return or exception
-        throw new RuntimeException();
-        //return null;
+        throw new ResourceException("timed out " + maxWaitMillis);
     }
 
-    public void unlockResource(Client client, T resource) {
-        resource.setBusy(false);
-        logger.debug(UNLOCK);
+    public void releaseOperator(Client client, T operator) {
+        operator.setBusy(false);
+        logger.debug(UNLOCK + operator);
+        System.out.println("Client #" + client.getId() + ": " + operator
+                + " --> disconnected");
         semaphore.release();
     }
 }
